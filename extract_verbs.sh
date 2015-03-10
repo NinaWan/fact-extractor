17758f1c107d4d5979cb8387c0436e1299be2465
NinaWan <aileenwnn@gmail.com>
2015-03-10 10:21:45 -0400

    Extract the raw text corpus from Chinese Wikipedia Chapter
    Just a beginner with NPL and Open source project, so the code may be not good enough. :-P
    But I'm interested in this GSoC project and hope I can make a contribution to it.
    I'm trying to run this script on my ubuntu system and see the results.
    
    The link of the Chinese parameter file and tokenizer created by Serge Sharoff is: http://corpus.leeds.ac.uk/tools/zh/

--- extract_verbs.sh
+++ extract_verbs.sh
@@ -3,20 +3,30 @@
 set -e
 
 cd ..
-# Download latest Italian Wikipedia dump
-wget http://download.wikimedia.org/itwiki/latest/itwiki-latest-pages-articles.xml.bz2
+
+# Check if the argument is null
+if [ ! -n "$1" ] ;then
+    echo "No Wikipedia dump URL entered!"
+    echo "Usage:"
+    echo "./extract_verbs.sh WikipediaDumpURL"   
+fi
+# Get the Wikimedia Dump URL
+wikimediaDumpURL=$1
+# Download latest Wikipedia dump
+wget -o wikipedia-dump.xml.bz2 $wikimediaDumpURL
 # Extract text
-bzcat itwiki-latest-pages-articles.xml.bz2 | scripts/lib/WikiExtractor.py
+bzcat wikipedia-dump.xml.bz2 | scripts/lib/WikiExtractor.py -o extracted/
 # Split extraction by article
+mkdir corpus
 cat extracted/*/* | csplit --suppress-matched -z -f 'corpus/doc_' - '/</doc>/' {*}
 # Build a single big file
 find extracted -type f -exec cat {} \; > all-extracted.txt
 # Extract verbs with TreeTagger
 # N.B. treetagger segfaults with the single big file, run it over each article instead
-#cat all-extracted.txt | treetagger/cmd/tree-tagger-italian | grep VER | sort -u > verbi.txt
-find extracted -type f -exec bash -c "cat '{}' | treetagger/cmd/tree-tagger-italian | grep VER >> verbi.txt" \;
+#cat all-extracted.txt | treetagger/cmd/tree-tagger-chinese | grep VER | sort -u > verbi.txt
+find extracted -type f -exec bash -c "cat '{}' | treetagger/cmd/tree-tagger-chinese | grep VER >> verbi.txt" \;
 sort -u verbi.txt > unique-sorted-verbs.txt
 # Extract vocabulary
 python scripts/bag_of_words.py all-extracted.txt
 # POS tagging + chunker with TextPro
-perl textpro.pl -verbose -html -l ita -c token+sentence+pos+chunk -o . ~/srl/training/itwiki/gold
+perl textpro.pl -verbose -html -l ita -c token+sentence+pos+chunk -o . ~/srl/training/zhwiki/gold
